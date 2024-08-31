af5848da02f91747ee84ced1d38e2a20a0598cb4
Filipe Vieira, Who? <filipevieirawho@gmail.com>
2024-08-31 00:24:57 +0000

    nova atualização de frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi, gatinha! Você tem mó rabão, fica tite não. ❤️</h1>
+    return <h1>Oi, gatinha! Fica tite não, você tem mó rabão 🍑❤️</h1>
 }
 
 export default Home;
